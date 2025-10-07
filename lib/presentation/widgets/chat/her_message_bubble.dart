1a52d48d85b238141fc6685a5492c434fb3edb43
Andres Rivero <andres19rivero@gmail.com>
2025-10-07 14:24:13 -0400

    feat: implement herMessageBubble with gif

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: colors.secondary,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hello World', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+        const SizedBox(height: 5),
+        _ImageBubble(),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadiusGeometry.circular(20),
+      child: Image.network(
+        'https://yesno.wtf/assets/no/12-dafd576be23d3768641340f76658ddfe.gif',
+        width: size.width * 0.7,
+        height: 150,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
