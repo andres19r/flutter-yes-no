16801ccacb170bd3ee98c5f0e16451339db761b1
Andres Rivero <andres19rivero@gmail.com>
2025-10-07 16:21:49 -0400

    fix: add focus and value behavior to MessageFieldBox widget

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,30 +5,40 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
       borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(40),
     );
 
     final inputDecoration = InputDecoration(
+      hintText: "End your message with a '?'",
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
         onPressed: () {
-          print('TextBox value');
+          final textValue = textController.value.text;
+          print('Button: $textValue');
+          textController.clear();
         },
         icon: const Icon(Icons.send_outlined),
       ),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('Submit value $value');
-      },
-      onChanged: (value) {
-        print('Changed: $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
