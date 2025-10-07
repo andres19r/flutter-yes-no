5202838e03e0b0648948a69bab21eabb8a94d1e3
Andres Rivero <andres19rivero@gmail.com>
2025-10-07 13:12:28 -0400

    feat(chat): add chatView widget as body of screen

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -10,11 +10,38 @@ class ChatScreen extends StatelessWidget {
         leading: const Padding(
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage('https://imgs.search.brave.com/jbPnB3G4mvSeerbe-Z_T2irBoGei9_81twAmzT5R0wc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/Z2V0LW1lLXBpY3R1/cmVzLW9mLXNwaWRl/ci1tYW4tdjAteTc2/YzR0dm16cGxkMS5q/cGc_d2lkdGg9NjQw/JmNyb3A9c21hcnQm/YXV0bz13ZWJwJnM9/YTIzMDJiNGE0YjE0/NmQ0MTE1OTY4Yjc3/MjAyNTg3NmM4Yzgx/NjRlMQ'),
+            backgroundImage: NetworkImage(
+              'https://imgs.search.brave.com/jbPnB3G4mvSeerbe-Z_T2irBoGei9_81twAmzT5R0wc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/Z2V0LW1lLXBpY3R1/cmVzLW9mLXNwaWRl/ci1tYW4tdjAteTc2/YzR0dm16cGxkMS5q/cGc_d2lkdGg9NjQw/JmNyb3A9c21hcnQm/YXV0bz13ZWJwJnM9/YTIzMDJiNGE0YjE0/NmQ0MTE1OTY4Yjc3/MjAyNTg3NmM4Yzgx/NjRlMQ',
+            ),
           ),
         ),
         title: Text('Hi Spidey'),
         centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Indice: $index');
+                },
+              ),
+            ),
+            Text('Mundo'),
+          ],
+        ),
       ),
     );
   }
