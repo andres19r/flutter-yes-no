120f011f98ae304bc43127a5c667128657ad9563
Andres Rivero <andres19rivero@gmail.com>
2025-10-16 08:58:18 -0400

    feat: implement model to map http response into a message

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  final ScrollController chatScrollController = ScrollController();
+  final chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messages = [
     Message(text: 'Hi Love!', fromWho: FromWho.me),
@@ -15,6 +17,17 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messages.add(newMessage);
 
+    if (text.endsWith('?')) {
+      herReply();
+    }
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messages.add(herMessage);
     notifyListeners();
     moveScrollToBottom();
   }
